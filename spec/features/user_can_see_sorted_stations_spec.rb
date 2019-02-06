b950d6a16903209b02f4126934c3493685960c4f
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-02-06 11:05:29 -0700

    Trying to figure out why i am not getting  a distance from my call.

--- spec/features/user_can_see_sorted_stations_spec.rb
+++ spec/features/user_can_see_sorted_stations_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
 describe "user" do
-  it "user can see stations by zip " do
-    
-    # As a user
+  it "user can see stations within a zip " do
     visit(root_path)
-    # When I visit "/"
 
     fill_in :q, with: 80203
-    # And I fill in the search form with 80203 (Note: Use the existing search form)
     click_button('Locate')
-    # And I click "Locate"
+
     expect(current_path).to eq(search_path)
-    # Then I should be on page "/search"
     expect(page).to have_content("Stations")
+    expect(page).to have_content("Station Name")
+    expect(page).to have_content("Station Address")
+    expect(page).to have_content("Station Fuel Type")
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     # And the stations should be limited to Electric and Propane
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
