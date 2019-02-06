1f8ca24e8095d795a3b6669e59fe5e314e5f8a15
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-02-06 10:46:26 -0700

    Working on station, getting station not initialized error

--- app/facade/stations_facade.rb
+++ app/facade/stations_facade.rb
@@ -1,12 +1,12 @@
 class StationsFacade
   def initialize(zip)
     @stations_data = StationService.new.get_stations(zip)
-
   end
 
   def stations
-    binding.pry
+    @stations_data.each do |station_data|
+      Station.new(station_data)
+      binding.pry
+    end
   end
-
-
 end
